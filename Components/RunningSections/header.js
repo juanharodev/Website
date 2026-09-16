91ac337519049351d9010393faa53d24af468190
Juan Haro <juan.haro.dev@gmail.com>
2026-09-16 17:03:08 -0600

    Add portfolio and home routing to header

--- Components/RunningSections/header.js
+++ Components/RunningSections/header.js
@@ -3,8 +3,8 @@ function createHeader(){
     header.innerHTML = `
         <p style="font-size: 18px;">Juan Haro</p>
         <nav class="header-navigation">
-            <a href="#">Home</a>
-            <a href="#">Portfolio</a>
+            <a href="/">Home</a>
+            <a href="/portfolio.html">Portfolio</a>
             <a href="#">Blog</a>
             <a href="#">Contact</a>
             <a href="#">About</a>
