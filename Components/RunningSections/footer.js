abba6bbd9f9661dacc218f87810e2ba9d9f039a9
Juan Haro <juan.haro.dev@gmail.com>
2026-09-17 19:00:13 -0600

    Translate main page to spanish

--- Components/RunningSections/footer.js
+++ Components/RunningSections/footer.js
@@ -1,7 +1,7 @@
 function createFooter(){
     const footer = document.createElement('footer');
     footer.innerHTML = `
-        <p> &copy; Juan Haro all right reserved ${new Date().getFullYear()}</p>
+        <p> &copy; Juan Haro todos los derechos reservados ${new Date().getFullYear()}</p>
     `;
 
     return footer;
--- Components/RunningSections/header.js
+++ Components/RunningSections/header.js
@@ -3,11 +3,8 @@ function createHeader(){
     header.innerHTML = `
         <p style="font-size: 18px;">Juan Haro</p>
         <nav class="header-navigation">
-            <a href="/">Home</a>
-            <a href="/portfolio">Portfolio</a>
-            <a href="#">Blog</a>
-            <a href="#">Contact</a>
-            <a href="#">About</a>
+            <a href="/">Inicio</a>
+            <a href="/portfolio">Portafolio</a>
         </nav>
     `;
 
